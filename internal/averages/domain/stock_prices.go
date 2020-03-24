d4c057b515a6e8751f2720ec20e35b65c28aa831
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2020-03-24 11:52:05 +0100

    Add stock prices to stock price sequences

--- internal/averages/domain/stock_prices.go
+++ internal/averages/domain/stock_prices.go
@@ -1,9 +1,25 @@
 package domain
 
+type PricePerDay struct {
+	Day   string
+	Price int
+}
+
 type StockPriceSequence struct {
-	Symbol string
+	Symbol      string
+	StockPrices []*PricePerDay
 }
 
 func NewStockPriceSequence(s string) *StockPriceSequence {
-	return &StockPriceSequence{Symbol: s}
+	return &StockPriceSequence{
+		Symbol:      s,
+		StockPrices: []*PricePerDay{},
+	}
+}
+
+func (s *StockPriceSequence) AddStockPrice(day string, price int) {
+	s.StockPrices = append(s.StockPrices, &PricePerDay{
+		Day:   day,
+		Price: price,
+	})
 }
--- internal/averages/domain/stock_prices_test.go
+++ internal/averages/domain/stock_prices_test.go
@@ -9,3 +9,20 @@ func Test_RepresentStockPriceSequence(t *testing.T) {
 	var sps *StockPriceSequence = NewStockPriceSequence("MSF")
 	assert.Equal(t, "MSF", sps.Symbol)
 }
+
+func Test_AddStockPriceSequence(t *testing.T) {
+	var sps *StockPriceSequence = NewStockPriceSequence("MSF")
+	sps.AddStockPrice("2020-02-20", 100)
+	assert.Equal(t, "2020-02-20", sps.StockPrices[0].Day)
+	assert.Equal(t, 100, sps.StockPrices[0].Price)
+}
+
+func Test_AddMultipleStockPrices(t *testing.T) {
+	var sps *StockPriceSequence = NewStockPriceSequence("MSF")
+	sps.AddStockPrice("2020-02-20", 100)
+	sps.AddStockPrice("2020-02-21", 110)
+	assert.Equal(t, "2020-02-20", sps.StockPrices[0].Day)
+	assert.Equal(t, 100, sps.StockPrices[0].Price)
+	assert.Equal(t, "2020-02-21", sps.StockPrices[1].Day)
+	assert.Equal(t, 110, sps.StockPrices[1].Price)
+}
