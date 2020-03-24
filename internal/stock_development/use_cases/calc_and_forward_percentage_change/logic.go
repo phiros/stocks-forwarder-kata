59e82f88d057391bb0f423065fd3cdce372fd162
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2020-03-24 13:58:39 +0100

    Fake first percentage calculation & added output ports

--- internal/stock_development/use_cases/calc_and_forward_percentage_change/logic.go
+++ internal/stock_development/use_cases/calc_and_forward_percentage_change/logic.go
@@ -1,6 +1,10 @@
 package calc_and_forward_percentage_change
 
-import "github.com/phiros/go-http-averages-kata/internal/stock_development/pkg/adapters/out/percentage_sink"
+import (
+	"github.com/phiros/go-http-averages-kata/internal/stock_development/domain"
+	"github.com/phiros/go-http-averages-kata/internal/stock_development/pkg/adapters/out/percentage_sink"
+	"github.com/shopspring/decimal"
+)
 
 type PercentageForwarder struct {
 	percentageSink percentage_sink.Port
@@ -8,4 +12,10 @@ type PercentageForwarder struct {
 
 func NewPercentageForwarder(percentageSink percentage_sink.Port) *PercentageForwarder {
 	return &PercentageForwarder{percentageSink: percentageSink}
+}
+
+func (f *PercentageForwarder) CalcAndForwardAsPercentages(stockPrices *domain.StockPrices) error {
+	f.percentageSink.Send(domain.NewStockPercentageChanges(stockPrices.Symbol).
+		AddPercentageChange("2020-02-21", decimal.NewFromFloat(10)))
+	return nil
 }
--- internal/stock_development/use_cases/calc_and_forward_percentage_change/logic_test.go
+++ internal/stock_development/use_cases/calc_and_forward_percentage_change/logic_test.go
@@ -2,19 +2,44 @@ package calc_and_forward_percentage_change
 
 import (
 	"github.com/phiros/go-http-averages-kata/internal/stock_development/domain"
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 type MockPercentageSink struct {
+	argumentRecorder []*domain.StockPercentageChanges
 }
 
-func (mps *MockPercentageSink) Send(_ domain.StockPercentageChanges) {
+func NewMockPercentageSink() *MockPercentageSink {
+	return &MockPercentageSink{argumentRecorder: []*domain.StockPercentageChanges{}}
+}
 
+func (mps *MockPercentageSink) invocations() int {
+	return len(mps.argumentRecorder)
+}
+
+func (mps *MockPercentageSink) Send(spc *domain.StockPercentageChanges) {
+	mps.argumentRecorder = append(mps.argumentRecorder, spc)
 }
 
 func Test_CreateNewPercentageForwarder(t *testing.T) {
 	mps := &MockPercentageSink{}
 	var pf *PercentageForwarder = NewPercentageForwarder(mps)
 	assert.Equal(t, pf.percentageSink, mps)
+}
+
+func Test_CalculateAndForwardChangeForTwoInputDays(t *testing.T) {
+	mps := NewMockPercentageSink()
+	var pf *PercentageForwarder = NewPercentageForwarder(mps)
+	prices := domain.NewStockPriceSequence("MSF").
+		AddStockPrice("2020-02-20", 100).
+		AddStockPrice("2020-02-21", 110)
+	err := pf.CalcAndForwardAsPercentages(prices)
+
+	assert.NoError(t, err)
+	assert.Equal(t, mps.invocations(), 1)
+	changes := mps.argumentRecorder[0].PercentageChange
+	assert.Equal(t, changes[0].Day, "2020-02-21")
+	assert.Equal(t, changes[0].ChangeInPercent, decimal.NewFromFloat(10))
 }
