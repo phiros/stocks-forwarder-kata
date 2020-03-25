0e5ee44442689831880bd437da548aed3152d20f
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2020-03-25 19:15:28 +0100

    Dockerized app & add mock server to test against

--- cmd/stocks-forwarder/main.go
+++ cmd/stocks-forwarder/main.go
@@ -4,11 +4,30 @@ import (
 	"github.com/phiros/go-http-averages-kata/internal/stock_development/pkg/adapters/in/prices"
 	"github.com/phiros/go-http-averages-kata/internal/stock_development/pkg/adapters/out/percentages"
 	"github.com/phiros/go-http-averages-kata/internal/stock_development/use_cases/calc_and_forward_percentage_change"
+	"os"
 )
 
+type Config struct {
+	percentageApiUrl string
+	pricesPath       string
+}
+
 func main() {
-	outPortToOtherService := percentages.NewHttpAPI("http://foo.invalid/changes")
+	config := Config{
+		percentageApiUrl: "http://foo.invalid/changes",
+		pricesPath:       "/prices",
+	}
+	pApi := os.Getenv("STOCKS_PERCENTAGE_API")
+	if len(pApi) > 0 {
+		config.percentageApiUrl = pApi
+	}
+	pricesPath := os.Getenv("STOCKS_PRICES_PATH")
+	if len(pricesPath) > 0 {
+		config.percentageApiUrl = pricesPath
+	}
+
+	outPortToOtherService := percentages.NewHttpAPI(config.percentageApiUrl)
 	useCase := calc_and_forward_percentage_change.NewPercentageForwarder(outPortToOtherService)
-	inPortHttpPricesApi := prices.NewHttpAdapter(useCase, "/prices")
+	inPortHttpPricesApi := prices.NewHttpAdapter(useCase, config.pricesPath)
 	inPortHttpPricesApi.Run()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/phiros/go-http-averages-kata
 go 1.14
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/shopspring/decimal v0.0.0-20200227202807-02e2044944cc
 	github.com/stretchr/testify v1.5.1
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
